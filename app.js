a559d204788dedb7331557d9fbf2d735bf48a1a2
Latoya <ilsimpson25@gmail.com>
2019-03-22 12:32:33 -0400

    final

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
 
-
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true}));
 
-const groceriesRoutes = require('./routes/groceries.js');
-
-// const PORT = process.env.PORT||3000
+const groceriesRoutes = require('./routes/groceries');
 
 app.use('/groceries', groceriesRoutes);
 app.use(function(req, res, next) {
@@ -25,13 +22,14 @@ app.get('/', (req, res) => {
     res.send({
         hello: "Hola my fellow compadres 🤪"
     })
-  })
+})
+
 
 MongoClient.connect('mongodb://sandbox:sandbox1@ds119930.mlab.com:19930/grocerylist', (err, database) => {
     if (err) return console.log(err)
     db = database.db('grocerylist')
-    app.listen(app.event.PORT || 3000, function () {
-        console.log('Listening on port 3000!', this.address().port, app.settings.env)
+    app.listen(process.env.PORT || 3000, function () {
+        console.log('Listening on port 3000!', this.address().port, app.settings.env);
     })
 })
 
